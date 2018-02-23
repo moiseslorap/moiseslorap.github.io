18fd35c939637a6781ed2d9a4426671ceb3e8c28
Moises Lora Perez (RIT Student) <mal3941@ad.rit.edu>
2018-02-22 21:06:48 -0500

    Added Beta for SD Project

---
+++ projects/sd/assets/scripts/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+function displayDiploma(){
+  document.getElementById("landing").style.display="none";
+  document.getElementById("diploma").style.display="block";
+}
+
+function saveInput(){
+
+}
+
+function showText(){
+  document.getElementById("diploma").style.display="none";
+  document.getElementById("text1").style.display="block";
+  setTimeout(function(){
+    document.getElementById("text1").style.display="none";
+    document.getElementById("text2").style.display="block";
+  },4000);
+}
