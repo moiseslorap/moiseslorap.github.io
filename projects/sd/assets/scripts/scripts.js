536365682e30a4bcd4cb8674745f611f2784990b
Moises Lora <mal3941@rit.edu>
2018-02-25 21:19:17 -0500

    Beta

--- projects/sd/assets/scripts/scripts.js
+++ projects/sd/assets/scripts/scripts.js
@@ -1,21 +1,58 @@
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
+$(function(){
+  // you should give id to form that is to be submitted.
+  // you had syntax error here. missing ) after function 
+  $("#my-form").submit(function(e) { // change # to .
+      var value = $("#name").val(); // you should have #input_name
+
+      $('#diploma-name').text(value); // text function takes value as parameter
+      e.preventDefault();
+  });
 });
 
 function displayDiploma(){
-  document.getElementById("landing").style.display="none";
-  document.getElementById("diploma").style.display="block";
-}
-
-function saveInput(){
-
+  $("#landing").hide();
+  $("#diploma").show();
+  $("#minilogo").show();
 }
 
 function showText(){
-  document.getElementById("diploma").style.display="none";
-  document.getElementById("text1").style.display="block";
+  printDiv();
+  $("#diploma").hide();
+  $("#text1").show();
   setTimeout(function(){
-    document.getElementById("text1").style.display="none";
-    document.getElementById("text2").style.display="block";
+    $("#text1").hide();
+    $("#text2").show();
   },6000);
+  setTimeout(function(){
+    $("#text2").hide();
+    $("#landing").show();
+  },10000);
+}
+
+$(function(){
+  // you should give id to form that is to be submitted.
+  // you had syntax error here. missing ) after function 
+  $("#my-form").submit(function(e) { // change # to .
+      var value = $("#name").val(); // you should have #input_name
+
+      $('#diploma-name').text(value); // text function takes value as parameter
+      e.preventDefault();
+  });
+});
+
+function printDiv() 
+{
+
+  var divToPrint=document.getElementById('ToPrint');
+
+  var newWin=window.open('','Print-Window');
+
+  newWin.document.open();
+
+  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+
+  newWin.document.close();
+
+  setTimeout(function(){newWin.close();},10);
+
 }
