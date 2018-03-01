625a97cd1a01f3f13c8e0bfc78166fc619e7ba0e
Moises Lora Perez (RIT Student) <mal3941@ad.rit.edu>
2018-03-01 01:17:55 -0500

    Done

--- projects/sd/assets/scripts/scripts.js
+++ projects/sd/assets/scripts/scripts.js
@@ -9,22 +9,32 @@ $(function(){
   });
 });
 
-$('#printDiv').on("click", function () {
-      $('#ToPrint').printThis({
-        importCSS: true
-      });
-    });
 
-
-function displayDiploma(){
+function displayTextInput(){
   $("#landing").hide();
-  $("#diploma").show();
+  $("#textinput").show();
   $("#minilogo").show();
 }
 
+function displayDiploma(){
+    $("#textinput").hide();
+    $("#minilogo").hide();
+    $("#diploma").show();
+    window.print();
+    setTimeout(function(){
+      showText();
+    },5000);
+}
+
+function printDiploma() {
+  //Hide all other elements other than printarea.
+  $("#diploma").show();
+};
+
 function showText(){
-  printDiv();
+  $("#textinput").hide();
   $("#diploma").hide();
+  $('#minilogo').show();
   $("#text1").show();
   setTimeout(function(){
     $("#text1").hide();
@@ -34,16 +44,48 @@ function showText(){
     $("#text2").hide();
     $("#minilogo").hide();
     $("#landing").show();
+    $('input').val('');
   },10000);
 }
 
-$(function(){
-  // you should give id to form that is to be submitted.
-  // you had syntax error here. missing ) after function 
-  $("#my-form").submit(function(e) { // change # to .
-      var value = $("#name").val(); // you should have #input_name
 
-      $('#diploma-name').text(value); // text function takes value as parameter
-      e.preventDefault();
-  });
+
+var CVS = document.createElement('canvas'),
+ctx = CVS.getContext('2d');
+
+CVS.width  = 1023;
+CVS.height = 790;
+document.getElementById("diploma").appendChild(CVS); // Add canvas to DOM
+
+// GRAPHICS TO CANVAS /////
+function sendToCanvas( ob ){
+var img = new Image();
+img.onload = function(){
+ctx.drawImage(img, 0, 0);
+ctx.font = ob.fontWeight+' '+ob.fontSize+' '+ob.fontFamily;
+ctx.textAlign = 'center';
+ctx.fillStyle = ob.color;
+ctx.fillText(ob.text, CVS.width/2, CVS.height/1.75);
+};
+img.src = ob.image;
+}
+///////////////////////////
+
+// DO IT! /////////////////
+
+var cvsObj = {
+image      : "assets/media/diploma.png",
+text       : "Please Enter Your Name",
+fontFamily : "Montserrat",
+fontWeight : "500",
+fontSize   : "20pt",
+color      : "black"
+};
+sendToCanvas( cvsObj );   
+
+
+
+$('input').on("input", function(){
+cvsObj.text = this.value ;
+sendToCanvas( cvsObj );
 });
